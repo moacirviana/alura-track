1dda3f05c164ac8791cddc103ea298f46c391ec7
moacirviana <moacir.viana@gmail.com>
2025-06-25 12:06:01 -0400

    composition API no projeto

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -1,8 +1,8 @@
 <template>
   <form @submit.prevent="salvar">
     <div class="field">
-      <label for="nomeDoProjeto" class="label"> Nome do Projeto</label>
-      <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjet" />
+      <label for="nomeDoProjeto" class="label"> Nome do Projeto {{ nomeDoProjeto }}</label>
+      <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto" />
     </div>
     <div class="field">
       <button class="button" type="submit">Salvar</button>
@@ -10,61 +10,47 @@
   </form>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import { useStore } from '@/store'
+import { useRouter } from 'vue-router'
 import { TipoNotificacao } from '@/interfaces/INotificacao'
-// import { notificacaoMixin } from '@/mixins/notificar'
 import useNotificador from '@/hooks/notificador'
 import { ALTERAR_PROJETO, CADASTRAR_PROJETO } from '@/store/tipo-acoes'
 
-export default defineComponent({
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Projetos',
-  props: {
-    id: {
-      type: String,
-    },
-  },
-  mounted() {
-    if (this.id) {
-      const projeto = this.store.state.projeto.projetos.find((proj) => proj.id === this.id)
-      this.nomeDoProjeto = projeto?.nome || ''
-    }
-  },
-  // mixins: [notificacaoMixin],
-  data() {
-    return {
-      nomeDoProjeto: '',
-    }
-  },
-  methods: {
-    salvar() {
-      if (this.id) {
-        this.store
-          .dispatch(ALTERAR_PROJETO, {
-            id: this.id,
-            nome: this.nomeDoProjeto,
-          })
-          .then(() => this.mensagemSucesso())
-      } else {
-        this.store
-          .dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto)
-          .then(() => this.mensagemSucesso())
-      }
-    },
-    mensagemSucesso() {
-      this.nomeDoProjeto = ''
-      this.notificar(TipoNotificacao.SUCESSO, 'Projeto salvo', 'Projeto foi salvo com sucesso!')
-      this.$router.push('/projetos')
-    },
-  },
-  setup() {
-    const store = useStore()
-    const { notificar } = useNotificador()
-    return { store, notificar }
-  },
-})
-</script>
+const props = defineProps<{
+  id?: string
+}>()
 
-<style scoped></style>
+const store = useStore()
+const { notificar } = useNotificador()
+const router = useRouter()
+
+const nomeDoProjeto = ref('')
+
+onMounted(() => {
+  if (props.id) {
+    const projeto = store.state.projeto.projetos.find((proj) => proj.id === props.id)
+    nomeDoProjeto.value = projeto?.nome || ''
+  }
+})
+
+function mensagemSucesso() {
+  nomeDoProjeto.value = ''
+  notificar(TipoNotificacao.SUCESSO, 'Projeto salvo', 'Projeto foi salvo com sucesso!')
+  router.push('/projetos')
+}
+
+function salvar() {
+  if (props.id) {
+    store
+      .dispatch(ALTERAR_PROJETO, {
+        id: props.id,
+        nome: nomeDoProjeto.value,
+      })
+      .then(() => mensagemSucesso())
+  } else {
+    store.dispatch(CADASTRAR_PROJETO, nomeDoProjeto.value).then(() => mensagemSucesso())
+  }
+}
+</script>
